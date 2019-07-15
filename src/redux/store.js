9e70680e68e68c1c727217accf9eaa3cf23cbb22
Adam Miller <adam@milrr.com>
2019-07-15 11:05:10 -0500

    add sessionstorage for cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const midddlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...midddlewares));
+export const store = createStore(rootReducer, applyMiddleware(...midddlewares));
 
-export default store;
+export const persistor = persistStore(store);
