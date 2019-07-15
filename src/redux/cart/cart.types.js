585ee5deebc3a4e966cb32e35d9bf9844f491e5f
Adam Miller <adam@milrr.com>
2019-07-15 09:31:52 -0500

    remove from cart

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 const CartActionTypes = {
   ADD_ITEM: 'ADD_ITEM',
-  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART'
 };
 
 export default CartActionTypes;
